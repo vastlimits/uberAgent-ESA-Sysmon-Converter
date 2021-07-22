27924c72513be2c3133ece83c50658bdf0915809
Denys Tsikota <denys@vastlimits.com>
2021-07-22 17:52:46 +0200

    Added reading of exclude registry event types

--- vl.Sysmon.Convert/Domain/Rules/Registry.cs
+++ vl.Sysmon.Convert/Domain/Rules/Registry.cs
@@ -20,20 +20,55 @@ namespace vl.Sysmon.Convert.Domain.Rules
             Log.Information("Converting rules for Registry..");
             var result = new List<ActivityMonitoringRule>();
 
-            foreach (var rule in registryEvent)
+            // Check in all rule groups if we exclude a rule like SetValue
+            var eventTypes = new List<EventType>
             {
-               var onMatch = rule.onmatch.ToLower();
+               EventType.RegKeyCreate, EventType.RegKeyDelete, EventType.RegValueWrite, EventType.RegKeyRename
+            };
+
+            var excludeList = from ruleGroup in registryEvent
+                      where ruleGroup.onmatch.Equals(Constants.SysmonExcludeOnMatchString)
+                      select ruleGroup.Items.Where(c => c.GetType() ==
+                                                        typeof(SysmonEventFilteringRuleGroupRegistryEventEventType))
+                                      .ToList()
+                      into toExcludeEventTypes
+                      from SysmonEventFilteringRuleGroupRegistryEventEventType item in toExcludeEventTypes
+                      select item;
+
+            foreach (var item in excludeList)
+            {
+               switch (item.Value)
+               {
+                  case "SetValue":
+                     eventTypes.Remove(EventType.RegValueWrite);
+                     break;
+                  case "CreateKey":
+                     eventTypes.Remove(EventType.RegKeyCreate);
+                     break;
+                  case "DeleteKey":
+                     eventTypes.Remove(EventType.RegKeyDelete);
+                     break;
+                  case "RenameKey":
+                     eventTypes.Remove(EventType.RegKeyRename);
+                     break;
+               }
+            }
+
+            foreach (var ruleGroup in registryEvent)
+            {
+               var onMatch = ruleGroup.onmatch.ToLower();
                if (!onMatch.Equals(Constants.SysmonExcludeOnMatchString) &&
                    !onMatch.Equals(Constants.SysmonIncludeOnMatchString))
                   continue;
 
+
                var activityConverterSettings = new ActivityMonitoringConverterSettings
                {
-                  EventType = new []{ EventType.RegKeyCreate, EventType.RegKeyDelete, EventType.RegValueWrite, EventType.RegKeyRename},
-                  Name = rule.name,
-                  Tag = rule.name,
-                  Conditions = ParseRule(rule).ToArray(),
-                  MainGroupRelation = rule.groupRelation
+                  EventType = eventTypes.ToArray(),
+                  Name = ruleGroup.name,
+                  Tag = ruleGroup.name,
+                  Conditions = ParseRule(ruleGroup).ToArray(),
+                  MainGroupRelation = ruleGroup.groupRelation
                };
 
                if (activityConverterSettings.Conditions.Length == 0)
