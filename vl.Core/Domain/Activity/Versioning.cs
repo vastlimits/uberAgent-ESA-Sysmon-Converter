29fba499792ef7a0ceabed4ee7c7bebca5cedaeb
Denys Tsikota <denys@vastlimits.com>
2023-11-13 19:37:04 +0200

    Event types are assigned to a minimum supported uberAgent version.

---
+++ vl.Core/Domain/Activity/Versioning.cs
@@ -0,0 +1,44 @@
+﻿namespace vl.Core.Domain.Activity;
+
+public static class Versioning
+{
+
+   public static bool IsSupportedByCurrentVersion(UAVersion version, EventType eventType)
+   {
+      switch (eventType)
+      {
+         case EventType.ProcessCreate:
+         case EventType.ProcessTerminate:
+         case EventType.ImageLoad:
+         case EventType.RegAny:
+         case EventType.RegKeyCreate:
+         case EventType.RegKeyDelete:
+         case EventType.RegValueWrite:
+         case EventType.RegKeyRename:
+            return version >= UAVersion.UA_VERSION_6_0;
+         case EventType.DnsQuery:
+            return version >= UAVersion.UA_VERSION_6_1;
+         case EventType.CreateRemoteThread:
+         case EventType.ProcessTampering:
+         case EventType.NetConnect:
+            return version >= UAVersion.UA_VERSION_6_2;
+
+         case EventType.FileCreate:
+         case EventType.FileDelete:
+         case EventType.FileRename:
+         case EventType.FileWrite:
+         case EventType.FileRead:
+         case EventType.FileCreateTime:
+         case EventType.FileCreateStreamHash:
+         case EventType.DriverLoad:
+         case EventType.FilePipeConnected:
+         case EventType.FilePipeCreate:
+         case EventType.RawAccessRead:
+            return version >= UAVersion.UA_VERSION_7_1;
+      }
+
+      return false;
+   }
+
+
+}
